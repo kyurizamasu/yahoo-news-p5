80dc94ee00ca4a66d74e269906a9a83008ea66dc
kyurizamasu <49762741+kyurizamasu@users.noreply.github.com>
2025-05-09 13:38:35 +0900

    Update sketch.js

--- client/sketch.js
+++ client/sketch.js
@@ -18,7 +18,7 @@ function draw() {
   for (let i = 0; i < headlines.length; i++) {
     let y = 40 + i * 24;
     let firstChar = headlines[i] ? headlines[i].charAt(0) : ''; // 安全に1文字取得
-    text(`${i + 1}: ${firstChar}`, 10, y);
+    text(` ${firstChar}`, 10, y);
   }
 }
 
